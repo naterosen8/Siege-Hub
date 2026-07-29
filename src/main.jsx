bad4198aecd94124540295e46b886cdd686314b5
naterosen8 <naterosen8@gmail.com>
2026-07-28 19:09:07 -0700

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SiegeHub from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SiegeHub />
+  </React.StrictMode>,
+)
